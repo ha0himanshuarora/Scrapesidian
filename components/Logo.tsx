eedf6a8a2fbac02b0e675905f1a32f1cb6ab4afb
ha0himanshuarora <ha0himanshuarora@gmail.com>
2025-03-08 12:51:41 +0530

    Modified Logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,13 +1,18 @@
 import { cn } from '@/lib/utils';
 import { SquareDashedMousePointer } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react'
+import Image from 'next/image';
+
 
 function Logo({fontSize="text-2xl",iconSize=20}:{fontSize?:string;iconSize?:number;}) {
     return (
     <div className={cn("text-2xl font-extrabold flex items-center gap-2", fontSize)}>
-        <div className="rounded-xl bg-gradient-to-r from-emerald-500 to-emerald-600 p-2">
-            <SquareDashedMousePointer size={iconSize} className="stroke-white" aria-label="Logo Icon" />
-        </div>
+      <Link className="flex font-bold flex-row p-2" href="/">
+        <Image src="/ObsidianLogo.png" width={40} height={40} alt="Obsidian logo" className="shadow-sm"/>
+      </Link>
+
+        
         <div>
             <span className="bg-gradient-to-r from-emerald-500 to-emerald-600 bg-clip-text text-transparent">
             Scrape
